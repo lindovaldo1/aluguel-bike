782aa7e8383e8dc565bb40972ff0bfbd384f9b10
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2023-03-29 12:52:56 -0300

    CRUD User
    all methods createds

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,9 +4,24 @@ module.exports = {
     async getAll(req, res){
         try {
             const user = await User.findAll({
-                where: {state: true}
+                where: { state: true }
             })
     
+            return res.status(200).json(user)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    },
+    async getById(req, res){
+        try { 
+            const { id } = req.params
+            const user = await User.findOne({
+                where: { id: Number(id), state: true }
+            })
+            
+            if(user === null)
+                return res.status(200).json("Nenhum usuário correspondente encontrado")
+
             return res.status(200).json(user)
         } catch (error) {
             return res.status(500).json(error.message)
@@ -17,7 +32,7 @@ module.exports = {
             const { name, email, password, date_birth } = req.body
 
             const user = await User.findOrCreate({
-                where: { email },
+                where: { name, email },
                 defaults:{ name, email, password, date_birth, state: true }
             })
             
@@ -27,7 +42,45 @@ module.exports = {
             return res.status(200).json(user)
         }catch(error){
             return res.status(500).json(error.message)
-        }
-        
+        }        
+    },
+    async update(req, res){
+        try{
+            const { id } = req.params
+            const { name, email, password, date_birth, state } = req.body
+            let user = await User.findOne({
+                where: { id: Number(id) }
+            })
+            
+            if(user === null)
+                return res.status(400).json("Usuário não encontrado")
+
+            await User.update({ name, email, password, date_birth, state }, { where: { id: Number(id) } })
+
+            user = await User.findOne({  where: { id: Number(id) } })
+
+            return res.status(200).json( user )
+        }catch(error){
+            return res.status(500).json(error.message)
+        }        
+    },
+    async delete(req, res){
+        try{
+            const { id } = req.params
+            let user = await User.findOne({
+                where: { id: Number(id), state: true}
+            })
+            
+            if(user === null)
+                return res.status(400).json("Usuário não encontrado")
+
+            await User.update({ state: false }, { where: { id: Number(id), state: true } })
+
+            user = await User.findOne({  where: { id: Number(id) } })
+
+            return res.status(200).json("Deletado com sucesso!")
+        }catch(error){
+            return res.status(500).json(error.message)
+        }        
     },
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,9 +5,9 @@ const userRoutes = Router()
 
 userRoutes
    .get('/', userController.getAll)
-   //.get('/:id',userController.getById)
-   .post('/',userController.create)
-//    .put('/:id',userController.update)
-//   .delete('/:id',userController.delete)
+   .get('/:id',userController.getById)
+   .post('/', userController.create)
+   .put('/:id',userController.update)
+   .delete('/:id',userController.delete)
 
 module.exports = userRoutes
