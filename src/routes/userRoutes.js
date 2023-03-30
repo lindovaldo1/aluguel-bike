49b1f0552e54050589f892d1b4da850e97a0d678
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2023-03-29 23:53:11 -0300

    typos users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,12 +2,13 @@ const { Router } = require('express')
 const userController = require('../controller/userController')
 
 const userRoutes = Router()
+const uri = '/users'
 
 userRoutes
-   .get('/', userController.getAll)
-   .get('/:id',userController.getById)
-   .post('/', userController.create)
-   .put('/:id',userController.update)
-   .delete('/:id',userController.delete)
+   .get(`${uri}/`, userController.getAll)
+   .get(`${uri}/:id`,userController.getById)
+   .post(`${uri}/`, userController.create)
+   .put(`${uri}/:id`,userController.update)
+   .delete(`${uri}/:id`,userController.delete)
 
 module.exports = userRoutes
